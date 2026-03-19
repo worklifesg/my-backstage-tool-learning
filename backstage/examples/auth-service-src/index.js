8a58ed4776a87e1cc2fbd5cc5c8f62104cf7fec2
Shraman Gupta <worklife_sg@outlook.com>
2026-03-18 22:03:18 -0400

    first commit

---
+++ backstage/examples/auth-service-src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
